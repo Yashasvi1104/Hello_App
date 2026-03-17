747664a301e530756f2d9f623b447fc5d0a14c8e
Yashasvi1104 <yashasviparihar1104@gmail.com>
2026-03-17 13:49:34 +0530

    uc2 done

--- HelloApp.java
+++ HelloApp.java
@@ -1,5 +1,7 @@
 public class HelloApp{
-    public static void main(String[] args) {
-        System.out.println("Hello World");
-    }
+	public static void main(String[] args){
+		String Name = args[0];
+		System.out.println("Hello " + Name + "!");
+	}
 }
+	
