bbb5d363ccc2c5184fbac7cee05985193622fbc7
Yashasvi1104 <yashasviparihar1104@gmail.com>
2026-04-21 14:00:15 +0530

    uc7 done

--- HelloApp.java
+++ HelloApp.java
@@ -1,15 +1,10 @@
-public class HelloSubstring {
+public class HelloStringJoin {
     public static void main(String[] args) {
         if (args.length == 0) {
             System.out.println("Hello, World!");
         } else {
-            String result = "Hello ";
-            for (String name : args) {
-                result += name + ", ";
-            }
-            // remove last ", "
-            result = result.substring(0, result.length() - 2);
-            System.out.println(result + "!");
+            String names = String.join(", ", args);
+            System.out.println("Hello " + names + "!");
         }
     }
 }
