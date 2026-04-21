b20c9688443bc84a9c12018b4aed715cc3bef6f6
Yashasvi1104 <yashasviparihar1104@gmail.com>
2026-04-21 13:43:36 +0530

    uc5_done

--- HelloApp.java
+++ HelloApp.java
@@ -1,15 +1,18 @@
-public class HelloApp{
+public class HelloEnhancedLoop {
     public static void main(String[] args) {
         if (args.length == 0) {
-            System.out.println("Hello World");
+            System.out.println("Hello, World!");
         } else {
             System.out.print("Hello ");
-            for (int i = 0; i < args.length; i++) {
-                System.out.print(args[i]);
-                if (i < args.length - 1) {
+            boolean first = true;
+            for (String name : args) {
+                if (!first) {
                     System.out.print(", ");
                 }
+                System.out.print(name);
+                first = false;
             }
+            System.out.println("!");
         }
     }
 }
